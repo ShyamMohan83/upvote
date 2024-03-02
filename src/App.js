007c5fdf506447d3455a5f88d31353fcf692f661
Shyam Mohan <mshyam83@gmail.com>
2024-03-02 17:58:28 +0530

    Task:Rename files and folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./component/Header/Header";
+import Header from "./components/Header/Header";
 import Home from './pages/Home';
 import './App.css';
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import {SecondaryHeading} from '../component/SecondaryHeading';
-import {Card} from '../component/Card';
+import {SecondaryHeading} from '../components/SecondaryHeading';
+import {Card} from '../components/Card';
 
 const Home = ()=> {
     return(
